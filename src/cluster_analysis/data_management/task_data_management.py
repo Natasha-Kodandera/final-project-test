dcd0a5b0d3ac2b4067597c0dc4dfacd93a901f37
Natasha_Kodandera <natashasomayya@gmail.com>
2026-03-06 21:11:24 +0530

    Edit task function to unzip data.

--- src/cluster_analysis/data_management/task_data_management.py
+++ src/cluster_analysis/data_management/task_data_management.py
@@ -2,19 +2,18 @@ import zipfile
 
 from cluster_analysis.config import BLD, SRC
 
+_PRODUCES = {
+    "marker": BLD / "data" / ".unzip_done",
+    "data": BLD / "data" / "cps_jan26.csv",
+    "variable_info": BLD / "data" / "cps_variable_info.csv",
+}
+
 
 def task_unzip_data(
     data=SRC / "data" / "raw" / "cps_data.zip",
-    produces=None,
-):
+    produces=_PRODUCES,
+) -> None:
     """Unzip the raw data file."""
-    if produces is None:
-        produces = {
-            "marker": BLD / "data" / ".unzip.done",
-            "cps": BLD / "data" / "cps_data.csv",
-            "info": BLD / "data" / "cps_variable_info.csv",
-        }
-
     out_path = BLD / "data"
     out_path.mkdir(parents=True, exist_ok=True)
     with zipfile.ZipFile(data, "r") as zip_ref:
