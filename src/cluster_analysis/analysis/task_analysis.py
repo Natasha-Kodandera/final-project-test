ab2b56bfcbad50ffc82f4d8819830f1e5333a9a2
Natasha_Kodandera <natashasomayya@gmail.com>
2026-03-11 01:28:16 +0530

    Add task function for cluster selection.

--- src/cluster_analysis/analysis/task_analysis.py
+++ src/cluster_analysis/analysis/task_analysis.py
@@ -4,10 +4,12 @@ from pathlib import Path
 import pandas as pd
 import pytask
 
+from cluster_analysis.analysis.cluster_selection import choose_n_clusters
 from cluster_analysis.analysis.clustering_model import fit_clustering_model
 from cluster_analysis.config import BLD, RANDOM_STATE, SRC
 
 N_CLUSTERS: int = 3
+N_CLUSTERS_TO_CHECK: tuple[int, ...] = (2, 3, 4, 5, 6)
 METHODS: tuple[str, ...] = ("kmeans", "agglomerative")
 
 for method in METHODS:
@@ -29,3 +31,26 @@ for method in METHODS:
         produces.parent.mkdir(parents=True, exist_ok=True)
         with produces.open("wb") as file:
             pickle.dump(model, file)
+
+
+def task_choose_n_clusters(
+    script: Path = SRC / "analysis" / "cluster_selection.py",
+    data: Path = BLD / "data" / "cps_clustering_data.feather",
+    produces: Path = BLD / "model_results" / "cluster_selection_scores.feather",
+) -> None:
+    """Evaluate and choose from different numbers of clusters for clustering method."""
+    df = pd.read_feather(data)
+
+    results = []
+    for method in METHODS:
+        for n_clusters in N_CLUSTERS_TO_CHECK:
+            result = choose_n_clusters(
+                data=df,
+                method=method,
+                n_clusters=n_clusters,
+                random_state=RANDOM_STATE,
+            )
+            results.append(result)
+
+    out = pd.concat(results, ignore_index=True)
+    out.to_feather(produces)
--- tests/analysis/test_clustering_model.py
+++ tests/analysis/test_clustering_model.py
@@ -4,13 +4,13 @@ from sklearn.cluster import AgglomerativeClustering, KMeans
 from cluster_analysis.analysis.clustering_model import fit_clustering_model
 
 
-def test_fit_clustering_model_kmeans():
+def test_fit_clustering_model_kmeans() -> None:
     df = pd.DataFrame({"x": [1, 3, 10, 13], "y": [1, 3, 10, 13]})
-    exp = fit_clustering_model(data=df, method="kmeans", n_clusters=2)
-    assert isinstance(exp, KMeans)
+    result = fit_clustering_model(data=df, method="kmeans", n_clusters=2)
+    assert isinstance(result, KMeans)
 
 
-def test_fit_clustering_model_agglomerative():
+def test_fit_clustering_model_agglomerative() -> None:
     df = pd.DataFrame({"x": [1, 3, 10, 13], "y": [1, 3, 10, 13]})
-    exp = fit_clustering_model(data=df, method="agglomerative", n_clusters=2)
-    assert isinstance(exp, AgglomerativeClustering)
+    result = fit_clustering_model(data=df, method="agglomerative", n_clusters=2)
+    assert isinstance(result, AgglomerativeClustering)
